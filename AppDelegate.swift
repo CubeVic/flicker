2d8a032e42ffbe1b413cbc6f8cbff1d1370c5cb7
Victor Fernandez <victoraguirre.f@gmail.com>
2016-03-12 23:18:04 +0700

    Update
    - [X] Add a tab bar for **Now Playing** and **Top Rated** movies.
    - [X] All images fade in.
    - [X] Customize the highlight and selection effect of the cell.
    - [X] Customize the navigation bar.

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -37,7 +37,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let tabBarController = UITabBarController()
         //put the number of view in the tab
         tabBarController.viewControllers = [nowPlayingNavigationController,topRatedNavigationController]
-        
+        tabBarController.tabBar.selectedImageTintColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        tabBarController.tabBar.barTintColor = UIColor(red: 0.0, green: 0.47, blue: 0.788, alpha: 1)
+        tabBarController.tabBar.translucent = true
+        tabBarController.tabBar.tintColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1)
         //set the tab bar in the root view controller
         window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
--- MoviesViewController.swift
+++ MoviesViewController.swift
@@ -14,6 +14,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
 
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var errorNetworkView: UIView!
     var movies: [NSDictionary]?
     var apiKeys = ("page","results")
     var apiKeysResultsKey = (poster_path:"poster_path",
@@ -38,11 +39,19 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let navigationBar = navigationController?.navigationBar
+        navigationBar?.tintColor = UIColor(red: 1.0, green: 0.5, blue: 0.5, alpha: 0.5)
+        navigationController?.navigationBar.tintColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
         tableView.dataSource = self
         tableView.delegate = self
         
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
         netWorkRequest()
+        
 
+        
         
     }
 
@@ -57,7 +66,9 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             delegateQueue:NSOperationQueue.mainQueue()
         )
         
+
         MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+       
         
         let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
             completionHandler: { (dataOrNil, response, error) in
@@ -69,6 +80,40 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                             self.tableView.reloadData()
                     }
                 }
+                else{
+                    MBProgressHUD.hideHUDForView(self.view, animated: true)
+                    self.errorNetworkView.hidden = false
+                }
+                
+        });
+        task.resume()
+    }
+    
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        
+        let clientId = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let serviceUrl = "https://api.themoviedb.org/3/movie/\(endPoint)?api_key=\(clientId)"
+        let url = NSURL(string: serviceUrl)
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            self.movies =  responseDictionary["results"] as! [NSDictionary]
+                            MBProgressHUD.hideHUDForView(self.view, animated: true)
+                            self.tableView.reloadData()
+                    }
+                }
+                // Tell the refreshControl to stop spinning
+                refreshControl.endRefreshing()	
         });
         task.resume()
     }
@@ -77,11 +122,26 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         return movies?.count ?? 0
     }
     
+    func imageFadeIn(view: UIView){
+        let movieCell = view as! MovieCell
+        UIView.beginAnimations("fade in", context: nil)
+        UIView.setAnimationDuration(1.5)
+        movieCell.posterView.alpha = 1
+        UIView.commitAnimations()
+    }
+
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
+        
+        //cell.selectionStyle = .None
+        let backgroundView = UIView(frame: cell.frame)
+        backgroundView.backgroundColor = UIColor(red: 0.0, green: 0.47, blue: 0.788, alpha: 0.8)
+        cell.selectedBackgroundView = backgroundView
+        cell.posterView.alpha = 0
         let results = getResults(indexPath)
         cell.titleLabel.text = results[apiKeysResultsKey.title] as! String
         cell.overviewLabel.text = results[apiKeysResultsKey.overview] as! String
+        imageFadeIn(cell)
         cell.posterView.setImageWithURL(results["imageUrl"] as! NSURL)
         return cell
     }
@@ -111,6 +171,17 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let detailsViewController = segue.destinationViewController as! DetailsViewController // set the destination
         detailsViewController.movies = results // give the info to the var in  detailsViewController
 
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        switch endPoint {
+        case "now_playing":
+            self.navigationItem.title = "Now Playing"
+        case "top_rated":
+            self.navigationItem.title = "Top Rated"
+        default:
+            self.navigationItem.title = "Movies"
+        }
     }
     
 
